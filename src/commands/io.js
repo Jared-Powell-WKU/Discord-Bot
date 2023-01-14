bb82f3e73fe1a456ca9c7f4f38c12b6214ae0ffd
Powell <jared.powell261@topper.wku.edu>
2023-01-14 15:24:34 -0600

    Made get/set bracket and io structure

---
+++ src/commands/io.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const tempPath = path.resolve(__dirname, "../json/temp.json");
+const commands = {
+    setbracket: {
+        data: new SlashCommandBuilder()
+            .setName("setbracket")
+            .addStringOption((option=>option.setName("link").setDescription("Allows moderator to update bracket link").setRequired(true)))
+            .setDescription("Updates bracket link")
+            .setDefaultMemberPermissions(PermissionsBitField.Flags.ModerateMembers),
+            async execute(interaction) {
+                var tmpObj = JSON.parse(fs.readFileSync(tempPath).toString())
+                tmpObj.bracket = interaction.options.getString("link");
+                fs.writeFileSync(tempPath, JSON.stringify(tmpObj));
+                await interaction.reply("Bracket updated");
+            }
+    },
+    bracket: {
+        data: new SlashCommandBuilder()
+            .setName("bracket")
+            .setDescription("Retrieves latest bracket"),
+            async execute(interaction) {
+                var tmpObj = JSON.parse(fs.readFileSync(tempPath).toString())
+                if(!tmpObj || !tmpObj.bracket) await interaction.reply("No bracket found")
+                else await interaction.reply(tmpObj.bracket)
+            }
+    }
+}
+module.exports = Object.keys(commands).map(name=>{return commands[name]});
--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
+    if(command.length) {
+        command.forEach(c => {
+            if('data' in c && 'execute' in c) client.commands.set(c.data.name, c)
+        });
     } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        if('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
 }
 
